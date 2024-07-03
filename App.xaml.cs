f5e3590c8873833a6166f18c1cb5b0a3ed1a282d
ClaudethMatute <150222554+ClaudethMatute@users.noreply.github.com>
2024-07-02 18:08:31 -0600

    Quitado de boton

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,14 +9,14 @@ namespace App_Cinopolis
             InitializeComponent();
 
             //MainPage = new AppShell();
-            // MainPage = new NavigationPage(new Models.pantalla_inicio());
+             MainPage = new NavigationPage(new Models.pantalla_inicio());
             //
             //MainPage = new NavigationPage(new Models.cinépolis_login());
 
             //  MainPage = new NavigationPage(new Models.restabelcer_contrasena());
            // MainPage = new NavigationPage(new Models.nueva_contrasena());
           //  MainPage = new NavigationPage(new Models.Verificar_contrasena());
-            MainPage = new NavigationPage(new Models.pantalla_registro());
+          //  MainPage = new NavigationPage(new Models.pantalla_registro());
         }
     }
 }
